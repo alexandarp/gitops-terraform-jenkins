27a79108834d2168b5874ac1f8452e19a0f80a18
mkzielak <49555821+mkzielak@users.noreply.github.com>
2019-07-30 23:31:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,11 +34,5 @@ resource "aws_security_group" "my-test-instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
 }
