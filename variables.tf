0ec8f59dc1d77e2bf319bf04fbce899c6ffede7a
jomara3 <jomara3@dxc.com>
2021-04-22 17:02:20 -0700

    Personalize the settings
    Personalize my repository settings

--- variables.tf
+++ variables.tf
@@ -4,17 +4,17 @@ variable "instance_count" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "terraform"
+  default     = "JoesKey"
 }
 
 variable "instance_type" {
   description = "AWS instance type"
-  default     = "t3.small"
+  default     = "t2.micro"
 }
 
 variable "ami" {
   description = "Base AMI to launch the instances"
 
   # Bitnami NGINX AMI
-  default = "ami-021acbdb89706aa89"
+  default = "ami-02dd1399544eb56a4"
 }
